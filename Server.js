d6da7551386e6d8daaee47756925e395dfe57467
kkarimwahba <karimwahba53@gmail.com>
2025-01-01 19:41:37 +0200

    `Added job and job form and contact us (schemes , controllers, &routes) and added to Server.js`

--- Server.js
+++ Server.js
@@ -11,6 +11,7 @@ const vendorRoutes = require("./routes/vendorRoutes");
 const catalogueRoutes = require("./routes/catalogueRoutes");
 const faqRoutes = require("./routes/faqRoutes");
 const policyRoutes = require("./routes/policiesRoutes");
+const contactUsRoutes = require("./routes/contactUsRoutes");
 const cors = require("cors");
 const path = require("path"); // For serving static files
 
@@ -73,6 +74,7 @@ app.use("/api/vendors", vendorRoutes);
 app.use("/api/catalogues", catalogueRoutes);
 app.use("/api/faqs", faqRoutes);
 app.use("/api/policies", policyRoutes);
+app.use("/api/contactus", contactUsRoutes); //contactUs Routes
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
