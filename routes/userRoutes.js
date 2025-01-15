b9372a6c5ea3745e8cfdfd3921d3799ce806ab04
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-01-15 20:03:44 +0200

    `Added new route for changing user password`

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -306,5 +306,51 @@ router.put("/updateUser", isAuthenticated, async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 });
+router.put("/changePassword", isAuthenticated, async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+
+  // // Check if the new password and confirm password match
+  // if (newPassword !== confirmPassword) {
+  //   return res
+  //     .status(400)
+  //     .json({ message: "New password and confirm password do not match" });
+  // }
+
+  // Validate password strength (optional)
+  if (newPassword.length < 8) {
+    return res
+      .status(400)
+      .json({ message: "Password should be at least 8 characters long" });
+  }
+
+  try {
+    // Find the user by their ID stored in the session
+    const user = await User.findById(req.session.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check if the current password matches the stored password
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Current password is incorrect" });
+    }
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update the user's password
+    user.password = hashedPassword;
+
+    // Save the updated user
+    await user.save();
+    console.log("Payload:", { currentPassword, newPassword });
+    console.log("Request body received by backend:", req.body);
+    res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+    console.error("Error changing password:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 module.exports = router;
