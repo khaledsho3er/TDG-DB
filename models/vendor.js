83d7ff49147e054eaa0a1e675779cdc3160e2313
khaledsho3er <khaledamrahmed0@gmail.com>
2025-01-29 20:07:04 +0200

    updated vendor with brand id

--- models/vendor.js
+++ models/vendor.js
@@ -6,10 +6,11 @@ const VendorSchema = new Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  employeeNumber: { type: String, required: true, unique: true },
+  employeeNumber: { type: String },
   phoneNumber: { type: String, required: false },
   tier: { type: String, enum: ["1", "2", "3"], default: "3" },
   role: { type: String, enum: ["Vendor"], default: "Vendor" },
   brandId: { type: mongoose.Schema.Types.ObjectId, ref: "Brand" },
 });
+
 module.exports = mongoose.model("Vendor", VendorSchema);
