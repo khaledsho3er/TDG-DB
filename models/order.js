d4b944be417a6a324a6fdd89bcfc3e19d44816f6
khaledsho3er <97366301+khaledsho3er@users.noreply.github.com>
2025-02-02 12:36:42 +0200

    Update billingInfo and order models: make cardNumber optional and rename phone to phoneNumber

--- models/order.js
+++ models/order.js
@@ -38,7 +38,7 @@ const orderSchema = new mongoose.Schema(
       lastName: { type: String, required: true },
       email: { type: String, required: true },
       address: { type: String, required: true },
-      phone: { type: String, required: true },
+      phoneNumber: { type: String, required: true },
       country: { type: String, required: true },
       city: { type: String, required: true },
       zipCode: { type: String, required: true },
