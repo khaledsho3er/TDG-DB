1f09650acfeda455e2b680e071aa3a1bce9baad2
khaledsho3er <97366301+khaledsho3er@users.noreply.github.com>
2025-02-16 15:31:49 +0200

    Fix Cloudflare R2 endpoint in multer setup for correct file upload configuration

--- middlewares/multerSetup.js
+++ middlewares/multerSetup.js
@@ -28,7 +28,7 @@ require("dotenv").config();
 // Cloudflare R2 Configuration
 const s3 = new S3Client({
   region: "auto", // Cloudflare R2 uses 'auto' for region
-  endpoint: "https://a29dbeb11704750c5e1d4b4544ae5595.r2.cloudflarestorage.com/images", // Replace with your actual R2 endpoint
+  endpoint: "https://a29dbeb11704750c5e1d4b4544ae5595.r2.cloudflarestorage.com", // Replace with your actual R2 endpoint
   credentials: {
     accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID,
     secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY,
