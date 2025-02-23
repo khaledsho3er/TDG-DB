4d612ca085a7a81ce407b771bbf00f41ed1ebf53
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-02-23 11:49:43 +0200

    Add description field to type schema in models

--- models/types.js
+++ models/types.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const typeSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  description: { type: String },
 });
 
 const Type = mongoose.model("types", typeSchema);
