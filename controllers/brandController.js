facfa8d6a7a77027cd5ac8434edd5b747b66606d
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-03-06 15:24:54 +0200

    Update brand logo and cover photo handling to use Cloudflare storage URLs

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -21,6 +21,8 @@ exports.createBrand = async (req, res) => {
       brandDescription,
       fees,
     } = req.body;
+    const baseURL =
+      "https://a29dbeb11704750c5e1d4b4544ae5595.r2.cloudflarestorage.com/images";
 
     const brand = new Brand({
       brandName,
@@ -39,14 +41,14 @@ exports.createBrand = async (req, res) => {
       brandDescription,
       fees,
       brandlogo: req.files["brandlogo"]
-        ? req.files["brandlogo"][0].filename
-        : "",
+        ? `${baseURL}/${req.files["brandlogo"][0].key}`
+        : null,
       digitalCopiesLogo: req.files["digitalCopiesLogo"]
         ? req.files["digitalCopiesLogo"].map((file) => file.filename)
         : [],
       coverPhoto: req.files["coverPhoto"]
-        ? req.files["coverPhoto"][0].filename
-        : "",
+        ? `${baseURL}/${req.files["coverPhoto"][0].key}`
+        : null,
       catalogues: req.files["catalogues"]
         ? req.files["catalogues"].map((file) => file.filename)
         : [],
