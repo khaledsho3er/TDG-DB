9810b19fdf411b05c79dd965efc5a1904808d54d
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-03-09 13:13:36 +0200

    Refactor getTypesForSubCategory function by removing commented-out code and adding debugging log for fetched types

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -74,41 +74,20 @@ exports.deleteType = async (req, res) => {
   }
 };
 
-// exports.getTypesForSubCategory = async (req, res) => {
-//   try {
-//     const subCategoryId = req.params.subCategoryId;
-//     console.log("Received SubCategory ID:", subCategoryId);
-
-//     // Find the subcategory and manually fetch the types
-//     const subCategory = await SubCategory.findById(subCategoryId);
-
-//     if (!subCategory) {
-//       return res.status(404).json({ message: "SubCategory not found" });
-//     }
-
-//     // Fetch types using the IDs stored in the subCategory's "types" field
-//     const types = await Type.find({ _id: { $in: subCategory.types } });
-
-//     res.status(200).json(types);
-//   } catch (error) {
-//     console.error("Error fetching types:", error);
-//     res.status(500).json({ message: "Error fetching types", error });
-//   }
-// };
-
 exports.getTypesForSubCategory = async (req, res) => {
   try {
     const subCategoryId = req.params.subCategoryId;
     console.log("Received SubCategory ID:", subCategoryId);
 
+    // Find the subcategory and manually fetch the types
     const subCategory = await SubCategory.findById(subCategoryId);
+
     if (!subCategory) {
       return res.status(404).json({ message: "SubCategory not found" });
     }
 
+    // Fetch types using the IDs stored in the subCategory's "types" field
     const types = await Type.find({ _id: { $in: subCategory.types } });
-
-    console.log("Fetched Types:", types); // Debugging line
 
     res.status(200).json(types);
   } catch (error) {
