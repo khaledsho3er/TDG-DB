e7b95a1348b3b56129b300cf6259147a00e068e3
khaledsho3er <karimwahba53@gmail.com>
2025-05-07 19:12:48 +0300

    Fix notification description to use customer name instead of customer ID in order creation

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -70,7 +70,7 @@ exports.createOrder = async (req, res) => {
     const brandId = updatedCartItems[0].brandId; // Get the brandId from the first item
     const newNotification = new Notification({
       type: "order",
-      description: `You have received a new order from customer ${customerId}.`,
+      description: `You have received a new order from customer ${customer}.`,
       brandId,
       orderId: savedOrder._id,
       read: false,
