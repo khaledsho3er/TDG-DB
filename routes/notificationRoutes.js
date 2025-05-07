75ab5df33d45aeadea5b211507f8ac299e1895a7
khaledsho3er <karimwahba53@gmail.com>
2025-05-07 18:48:00 +0300

    Add admin route to fetch all notifications with brand details

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Notification = require("../models/notification"); // Import the Notification model
 const notificationController = require("../controllers/notificationController");
-
+const Brand = require("../models/Brand"); // Import the Brand model
 // GET notifications for the brand
 router.get("/notifications", async (req, res) => {
   try {
@@ -16,7 +16,18 @@ router.get("/notifications", async (req, res) => {
     res.status(500).json({ message: "Error fetching notifications" });
   }
 });
+router.get("/admin/all-notifications", async (req, res) => {
+  try {
+    const notifications = await Notification.find()
+      .sort({ date: -1 }) // Sort notifications by date
+      .populate("brandId", "brandName brandlogo email"); // Populate brand details
 
+    res.json(notifications);
+  } catch (error) {
+    console.error("Error fetching all notifications:", error);
+    res.status(500).json({ message: "Error fetching notifications" });
+  }
+});
 router.patch("/:id/mark-as-read", notificationController.markAsRead);
 
 module.exports = router;
