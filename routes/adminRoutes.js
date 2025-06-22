35b9ba8dcefa9648bb03575a2ac5e573afef9ee1
khaledsho3er <karimwahba53@gmail.com>
2025-06-22 13:31:38 +0300

    Remove authentication middleware from admin routes for retrieving all admins and updating/deleting profiles

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -8,10 +8,10 @@ router.post("/register", adminController.signupAdmin);
 router.post("/login", adminController.loginAdmin);
 router.get("/profile", authMiddleware, adminController.getAdminProfile);
 //get all admins
-router.get("/all-admins", authMiddleware, adminController.getAllAdmins);
+router.get("/all-admins", adminController.getAllAdmins);
 // Update admin profile
-router.put("/profile/:id", authMiddleware, adminController.updateAdmin);
+router.put("/profile/:id", adminController.updateAdmin);
 // Delete admin profile
-router.delete("/profile/:id", authMiddleware, adminController.deleteAdmin);
+router.delete("/profile/:id", adminController.deleteAdmin);
 
 module.exports = router;
