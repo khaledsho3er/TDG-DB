99893b364f1f8129aa87ffa5fd9db8f9fdcd5884
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-07-07 17:36:14 +0300

    Add updateProductByAdmin import: include the updateProductByAdmin function in productsRoutes for enhanced product management capabilities.

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -25,6 +25,7 @@ const {
   approvePendingUpdate,
   rejectPendingUpdate,
   getPendingProducts,
+  updateProductByAdmin,
 } = require("../controllers/productsController");
 
 const router = express.Router();
